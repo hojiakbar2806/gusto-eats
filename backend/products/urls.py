e0ac84413c23285ed6912ddad2e108bb5de2a3a2
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2025-01-28 21:11:25 +0500

    fix

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -9,13 +9,13 @@ router.register(r'categories', products.CategoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('orders/add/', orders.add_order_items, name='add_order_items'),
-    path('myorders/', orders.getMyOrders, name='get_my_orders'),
-    path('orders/', orders.getOrders, name='get_orders'),
-    path('orders/<int:pk>/paid/', orders.updateOrderToPaid, name='update_order_to_paid'),
-    path('orders/<int:pk>/delivered/', orders.updateOrderToDelivered, name='update_order_to_delivered'),
-    path('orders/<int:pk>/', orders.getOrderById, name='get_order_by_id'),
+    path('orders/add', orders.add_order_items, name='add_order_items'),
+    path('myorders', orders.getMyOrders, name='get_my_orders'),
+    path('orders', orders.getOrders, name='get_orders'),
+    path('orders/<int:pk>/paid', orders.updateOrderToPaid, name='update_order_to_paid'),
+    path('orders/<int:pk>/delivered', orders.updateOrderToDelivered, name='update_order_to_delivered'),
+    path('orders/<int:pk>', orders.getOrderById, name='get_order_by_id'),
 
-    path('admin/stats/', misk.AdminStatsView.as_view(), name='admin-stats'),
-    path('feedback/', misk.FeedbackAPIView.as_view())
+    path('admin/stats', misk.AdminStatsView.as_view(), name='admin-stats'),
+    path('feedback', misk.FeedbackAPIView.as_view())
 ]
