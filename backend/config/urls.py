036f3dbcb01907f4e68468d1d829414c0f93e0e8
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2025-01-28 20:58:12 +0500

    fix swagger

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/accounts/', include("accounts.urls")),
     path('api/', include("products.urls")),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 if settings.DEBUG:
