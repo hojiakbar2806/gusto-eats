dad6293c502419306ea38636c7124244dfb3c4ca
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2025-01-28 20:52:19 +0500

    fix admin url

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,7 +21,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/accounts/', include("accounts.urls")),
     path('api/', include("products.urls")),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
